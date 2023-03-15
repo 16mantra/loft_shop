f7cd1df7a90bdb5aa517eb2ce83aee6f82d2880f
Arsen Sanamyan <arsensanamyan2017@gmail.com>
2023-03-15 20:53:29 +0300

    Работа с админ панелью. Добавил странциу добавления слайдера и основную странциу слайдера. Напсиал контроллер для получения данных с дб

---
+++ admin/pages/slider_index.php
@@ -0,0 +1,22 @@
+<?php
+var_dump(get_data());
+
+?>
+<div class="row mb-3">
+    <div class="add">
+        <a class="btn btn-primary" href="slider_add">Add new picture</a>
+    </div>
+</div>
+<div class="row">
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
+
+
